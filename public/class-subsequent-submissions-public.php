174055eca3503bb17d14f8133a783a223d78f4e5
David Powers <david@1xrun.com>
2016-02-25 17:13:40 -0500

    deleted unused code. added documentation.

--- public/class-subsequent-submissions-public.php
+++ public/class-subsequent-submissions-public.php
@@ -1,35 +1,25 @@
 <?php
 require("TestSubsequentSubmissions.php");
-class UserHelperII {
-	const PRIMARY_NUM_KEY = "mobile_phone";
-	static public function findUser($pet_owner_id) {
-		$user = false;
-		$query = new WP_User_Query( array( 'meta_key' => 'pet_owner_id', 'meta_value' => $pet_owner_id ) );
-		if (count($query->results) == 1) {
-			$user = $query->results[0];
-		}
-		return $user;
-	}
-	static public function guardianMobileKey($petNum,$guardianNum) {
-		return "p{$petNum}_guardian_{$guardianNum}_mobile_phone";
-	}
-	static public function getGuardianNumber($userId,$petNum,$guardianNum) {
-		$meta = get_metadata('user', $userId);
-		$key = UserHelper::guardianMobileKey($petNum,$guardianNum);
-		return $meta[$key][0];
-	}
-	static public function updateGuardianNumber($userId,$petNum,$guardianNum,$newNum) {
-		$key = UserHelper::guardianMobileKey($petNum,$guardianNum);
-		update_user_meta( $userId, $key, $newNum );
-		return UserHelper::getGuardianNumber($userId,$petNum,$guardianNum);
-	}
-}
+/**
+ * KillItem is a utility class that works with the Notify class,
+ * to provide a list of notifications that should be blocked.
+ */
 class KillItem {
 	public $name, $to;
+	/**
+	 * @param string $name    Name of the notification to be blocked.
+	 * @param string $to      Email of the person being notified.
+	 */
 	public function __construct($name,$to) {
 		$this->name = $name;
 		$this->to = $to;
 	}
+	/**
+	 * The killReminders method is designed for the situation when a pet is new,
+	 * and all reminders for the pet should be blocked at once
+	 * @param  string $petnum   Number of the petfile (1-5) for the current pet
+	 * @return array            An array containing all the KillItem objects.
+	 */
 	static public function killReminders($petnum) {
 		$arr = array();
 		for($g=1;$g<6;$g++) {
@@ -38,6 +28,14 @@ class KillItem {
 		return $arr;
 	}	
 }
+/**
+ * Notify class is used to actually block notifications. It is initialized
+ * with data from a single notification, and is designed to run on indivual 
+ * notifications before they are sent. When the $kill_list attribute is set 
+ * to an array of KillItem objects, and the block() method is called, it will 
+ * run through the list and if the current notification matches anything in
+ * the list, the notification will not be sent. 
+ */
 class Notify {
 	public $kill_list;
 	public function __construct($notification) {
@@ -65,38 +63,56 @@ class Notify {
 		return(json_encode($obj));
 	}
 }
+/**
+ * SubmissionsHelper class provides some simple utilities for getting meta
+ * data, including checking number of pets, and whether a pet is new.
+ */
 class SubmissionsHelper {
+	/**
+	 * numberPets finds the number of pets when given the user meta data 
+	 * @param  array $meta   An array containing the WP user meta data
+	 * @return string        Returns value of how_many_pets_owned user meta
+	 */
 	static public function numberPets($meta) {
 		return SubmissionsHelper::meta($meta,'how_many_pets_owned');
 	}
+	/**
+	 * petOwnerId finds the pet owner id when given the user meta data 
+	 * @param  array $meta   An array containing the WP user meta data
+	 * @return string        Returns pet owner id
+	 */
 	static public function petOwnerId($meta) {
 			return rgar(rgar($meta,'pet_owner_id'),0);
 	}
+	/**
+	 * meta() allows one to retrieve an arbitrary field of user meta data
+	 * @param  array $meta     An array containing the WP user meta data
+	 * @param  string $field   Key for user meta data 
+	 * @return string          Returns value of user meta if $field exists
+	 */
 	static public function meta($meta,$field) {
 		return rgar(rgar($meta,$field),0);
 	}
+	/**
+	 * isPetNew uses the pet name field in user meta to check if pet exists.
+	 * @param  string $petnum     Which pet is it? (1-5)
+	 * @param  array $meta        An array containing the WP user meta data
+	 * @return boolean
+	 */
 	static public function isPetNew($petnum,$meta) {
 		$metaname = SubmissionsHelper::meta($meta,"pet_{$petnum}_name");
 		//is pet name in the user meta?
 		return ($metaname=="");
 	}
-	static public function isGuardianNew() {
-		return false;
-	}
-	static public function hasGuardianResponded() {
-		return false;
-	}
 
 }
 
 /**
- * The public-facing functionality of the plugin.
- *
- * @link       http://rezon8.net/
- * @since      1.0.0
- *
- * @package    Subsequent_Submissions
- * @subpackage Subsequent_Submissions/public
+ * Pet2 is a utility class that represents pet and guardian data.
+ * It abstract the details of pets and guardian, making it easy to loop
+ * through and test all the guardians. It also provides utility functions
+ * for taking gravity form data, and converting it to use the same form
+ * as the user meta data, making it easy to compare the two. 
  */
 
 class Pet2 {
@@ -104,9 +120,22 @@ class Pet2 {
 	public function __construct( $petfile ) {
 		$this->petfile = $petfile;
 	}
+	/**
+	 * setGuardian is used when to store minimal data about the guardian with
+	 * the Pet2 object, in this case just the email.
+	 * @param integer $guardianNum   Which guardian is it?
+	 * @param string $email          The guardian's email
+	 */
 	public function setGuardian($guardianNum,$email) {
 		$this->guardians[$guardianNum] = new Guardian2($email);
 	}
+	/**
+	 * post2Data converts data from the Add Pets & Guardians form
+	 * and changes it into a form where it can be compared with the user
+	 * meta data. 
+	 * @param  $post array   Array containing post data, normally $_POST
+	 * @return array         Returned array has keys named to match user meta
+	 */
 	static public function post2Data($post) {
 		$data = [];
 		$data['how_many_pets_owned'] = (int) rgar($post,'input_59');
@@ -135,6 +164,16 @@ class Pet2 {
 		}
 		return $data;
 	}
+	/**
+	 * petfilePost2Data lets you take form data from the update petfile
+	 * and change it into a form where it can be compared with the user
+	 * meta data. If you need to block notifications from some other form,
+	 * you will want to right a function like this, only rewrite to use 
+	 * the correct gravity form ID numbers in the $formFields array
+	 * @param  string $petNum    Which petfile (1-5) is being updated
+	 * @param  array $post       Form data, usually $_POST unless unit testing
+	 * @return array             Array with keys that match user meta keys
+	 */
 	static public function petfilePost2Data($petNum,$post) {
 		$formFields = array(
 			"p{$petNum}_guardian_1_email"=>'167',
@@ -148,11 +187,15 @@ class Pet2 {
 		}
 		return $data;
 	}
-	static public function data2Pets($data) {
-		for($p=1;$p<$data['how_many_pets_owned'];$p++) {
-		}
-		return $pets;
-	}
+	/**
+	 * getPet is used to model pets and guardians in the submitted form data
+	 * The objects created this way will be used in checkGuardianNotifications
+	 * and compared against objects created from the user meta data, to check
+	 * if guardians are new based on email. 
+	 * @param  string $petNum   which pet (1-5)
+	 * @param  array $data      user meta data, or array with same format
+	 * @return Pet2 object      returns a Pet2 object with guardian info set
+	 */
 	static public function getPet($petNum,$data) {
 		$pet = new Pet2($petNum);
 		$num = (int) $data["num_guardians_p{$petNum}"];
@@ -160,9 +203,19 @@ class Pet2 {
 		for($g=1;$g<6;$g++) {
 			$f = "p{$petNum}_guardian_{$g}_email";
 			$pet->setGuardian($g,$data[$f]);
-		}
+		} 
 		return $pet;
 	}
+	/**
+	 * checkGuardianNotifications is creates a list of blocked notifications
+	 * from two versions of pet data, one from meta, and one from a form.
+	 * This function is used when forms are submitted that update
+	 * guardian information, for a pet that is not new.
+	 * @param  Pet object $pet      (created from meta)
+	 * @param  Pet2 object $newPet  (created from form data)
+	 * @param  array $killArr       (list of blocked notifications)
+	 * @return array                (updated list of blocked notifications)
+	 */
 	static public function checkGuardianNotifications($pet,$newPet,$killArr) {
 		$g = 1;
 		foreach ($pet->guardians as $guardian) {
@@ -193,6 +246,10 @@ class Pet2 {
 	}
 
 }
+/**
+ * Guardian2 is a utility class that uses only the Guardian email data.
+ * (Use original Guardian class for full representation of Guardian data.)
+ */
 class Guardian2 {
 	public $email;
 	public function __construct( $email ) {
@@ -242,7 +299,11 @@ class Subsequent_Submissions_Public {
 		$this->version = $version;
 
 	}
-
+	/**
+	 * filter_add_pets runs when the Add Pets & Guardians form is submitted
+	 * @param  array $form   gravity forms form data
+	 * @return n/a
+	 */
 	public function filter_add_pets($form) {
 		$user = wp_get_current_user();
 		$meta = get_metadata('user', $user->ID);
@@ -272,11 +333,18 @@ class Subsequent_Submissions_Public {
 		$_POST['input_239'] = $json;
 		GFCommon::log_debug( __METHOD__ . '(): logging Add Pet json-kill-list: '.print_r($json, true) );
 	}
-
+/**
+ * add_pet_notification sets the notification blocking filter to run on the 
+ * Add Pets & Guardians form, and provides the hidden field ID 239
+ */
 	public function add_pet_notification($notification, $form, $entry) {
 		$this->filter_notifications('239',$notification,$entry);
 		return $notification;
 	}
+/**
+ * filter_petfile1 checks data submitted with petfile1, and writes json data
+ * to form field 205 to be decoded in notification phase
+ */
 	public function filter_petfile1($form) {
 		$this->filter_petfile(1,205);
 	}
@@ -292,20 +360,37 @@ class Subsequent_Submissions_Public {
 	public function filter_petfile5($form) {
 		$this->filter_petfile(5,205);
 	}			
+/**
+ * petfile1_notification sets the notification blocking filter to run on the 
+ * Petfile1 form, and provides the hidden field ID 205
+ */
 	public function petfile1_notification($notification, $form, $entry) {
 		$this->filter_notifications('205',$notification,$entry);
 		return $notification;
 	}
+	/**
+	 * filter_petfile was designed to be run when petfile forms are updated,
+	 * to block guardian reminders and requests as needed.
+	 * @param  [type]
+	 * @param  [type]
+	 * @return [type]
+	 */
 	public function filter_petfile($petNum,$hiddenFieldId) {
+		//get the current user and their metadata
 		$user = wp_get_current_user();
 		$meta = get_metadata('user', $user->ID);
 		GFCommon::log_debug( __METHOD__ . '(): logging Meta from Petfile Form: '.print_r($meta, true) );
 		$pet_owner_id = SubmissionsHelper::petOwnerId($meta);	
+		//Change the submitted form data to a format that matches user meta data
+		//change the following line if rewriting to grab data from other forms...
 		$data = Pet2::petfilePost2Data($petNum,$_POST);
 		GFCommon::log_debug( __METHOD__ . '(): logging Parsed Data from Petfile Form: '.print_r($data, true) );
 		$killArr = [];
+		//is it a new pet? If so, just kill all guardian reminders at once
 		if(SubmissionsHelper::isPetNew($petNum,$meta)) {
 			$killArr = array_merge($killArr,KillItem::killReminders($petNum));
+		//otherwise, we need to create the Pet and Pet2 objects, and compare with
+		//Pet2::checkGuardianNotifications()
 		} else {
 			$pet = Pet::getPet($pet_owner_id,$petNum,$meta);			
 			GFCommon::log_debug( __METHOD__ . '(): Pet Object from Petfile Form: '.print_r($pet, true) );
@@ -318,6 +403,14 @@ class Subsequent_Submissions_Public {
 		$_POST[$key] = $json;
 		GFCommon::log_debug( __METHOD__ . '(): logging Add Pet json-kill-list: '.print_r($json, true) );
 	}
+	/**
+	 * filter_notifications can be run on any form where you wrote 
+	 * json kill list data to a hidden field, to block unwanted notifications
+	 * @param  string $field        Gravity entry ID for hidden json field
+	 * @param  array $notification  Gravity forms, notification data
+	 * @param  array $entry         Gravity forms, form entry data
+	 * @return [type]
+	 */
 	public function filter_notifications($field,$notification,$entry) {
 		$n = new Notify($notification);
 		$k = json_decode($entry[$field]);
